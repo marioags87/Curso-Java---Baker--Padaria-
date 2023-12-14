1c754c67b34347e97f97fcb659efefb4393c6074
marioags87 <marioandersongs@gmail.com>
2023-12-14 20:26:20 -0300

    Entity Product

---
+++ bakery/src/main/java/com/mario/bakery/model/Product.java
@@ -0,0 +1,29 @@
+package com.mario.bakery.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_product")
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "product_name")
+    private String name;
+
+    private int quantity;
+    
+    private double price;
+    
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
